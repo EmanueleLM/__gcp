6572eac74917718f8c9ca6f85a4536def1904d31
Emanuele <emanuele.lamalfa@mail.polimi.it>
2020-04-01 00:07:35 +0200

    CNNs and fc analysis on server + bug solved on link weights plot.

--- experiments/imdb/unified_analysis/server_plot_generator.py
+++ experiments/imdb/unified_analysis/server_plot_generator.py
@@ -43,10 +43,9 @@ for acc, i in zip(results_folders, range(num_colors)):
         w = np.load(file_, allow_pickle=True)
         total_weights = np.append(w[-2].flatten(), total_weights)
         total_bias = np.append(w[-1].flatten(), total_bias) 
-        density = stats.kde.gaussian_kde(np.concatenate((total_weights.flatten(), total_bias.flatten())))
-        x = np.arange(-1., 1., .01)
-        plt.plot(x, density(x), alpha=.5, color=str(colors[i]))
-        #plt.hist(np.concatenate((total_weights.flatten(), total_bias.flatten())), bins=bins, alpha=.5, color=str(colors[i]), histtype='step', cumulative=False, normed=False)
+    density = stats.kde.gaussian_kde(np.concatenate((total_weights.flatten(), total_bias.flatten())))
+    x = np.arange(-1., 1., .01)
+    plt.scatter(x, density(x), alpha=.5, color=str(colors[i]),s=1)
 plt.savefig('./images/{}/histogram_total_weights-accuracy({}-{}-step-{}).png'.format(topology,0.1, 1.0, step))
 plt.show()
 plt.close() 
@@ -64,8 +63,7 @@ for acc, i in zip(results_folders, range(num_colors)):
     s_input_layer = total_weights.reshape(n_files, 32, 2).sum(axis=-1)
     density = stats.kde.gaussian_kde(s_input_layer.flatten())
     x = np.arange(s_input_layer.min(), s_input_layer.max(), .001)
-    plt.plot(x, density(x), alpha=.5, color=str(colors[i]))
-    #plt.hist(s_input_layer.flatten(), bins=bins, alpha=.5, color=str(colors[i]), histtype='step', cumulative=False, normed=False)
+    plt.scatter(x, density(x), alpha=.5, color=str(colors[i]), s=1)
 plt.savefig('./images/{}/histogram_total_node_strenght_input-layer-accuracy({}-{}-step-{}).png'.format(topology,0.1, 1.0, step))
 plt.show()
 plt.close()
@@ -83,8 +81,7 @@ for acc, i in zip(results_folders, range(num_colors)):
     s_output_layer = total_weights.reshape(n_files, 32, 2).sum(axis=1) + total_bias.reshape(n_files, 2,)
     density = stats.kde.gaussian_kde(s_output_layer.flatten())
     x = np.arange(s_output_layer.min(), s_output_layer.max(), .001)
-    plt.plot(x, density(x), alpha=.5, color=str(colors[i]))
-    #plt.hist(s_output_layer.flatten(), bins=bins, alpha=.5, color=str(colors[i]), histtype='step', cumulative=False, normed=False)
+    plt.scatter(x, density(x), alpha=.5, color=str(colors[i]), s=1)
 plt.savefig('./images/{}/histogram_total_node_strenght_output-layer-accuracy({}-{}-step-{}).png'.format(topology,0.1, 1.0, step))
 plt.show()
 plt.close()
@@ -105,8 +102,7 @@ for acc, i in zip(results_folders, range(num_colors)):
     Y = np.sum(total_weights.reshape(n_files, 32, 2)**2, axis=-1)/s_input_layer**2
     density = stats.kde.gaussian_kde(Y.flatten())
     x = np.arange(Y.min(), Y.max(), .001)
-    plt.plot(x, density(x), alpha=.5, color=str(colors[i]))
-    #plt.hist(Y.flatten(), bins=bins, alpha=.5, color=str(colors[i]), histtype='step', cumulative=False, normed=False)
+    plt.scatter(x, density(x), alpha=.5, color=str(colors[i]), s=1)
 plt.savefig('./images/{}/histogram_total_node_disparity_input-layer-accuracy({}-{}-step-{}).png'.format(topology,0.1, 1.0, step))
 plt.show()
 plt.close()
@@ -127,8 +123,7 @@ for acc, i in zip(results_folders, range(num_colors)):
     Y = (np.sum(total_weights.reshape(n_files, 32, 2)**2, axis=1)+total_bias.reshape(n_files, 2,)**2)/s_output_layer**2
     density = stats.kde.gaussian_kde(Y.flatten())
     x = np.arange(Y.min(), Y.max(), .0001)
-    plt.plot(x, density(x), alpha=.5, color=str(colors[i]))
-    #plt.hist(Y.flatten(), bins=bins, alpha=.5, color=str(colors[i]), histtype='step', cumulative=False, normed=False)
+    plt.scatter(x, density(x), alpha=.5, color=str(colors[i]), s=1)
 plt.savefig('./images/{}/histogram_total_node_disparity_output-layer-accuracy({}-{}-step-{}).png'.format(topology,0.1, 1.0, step))
 plt.show()
 plt.close()
@@ -147,7 +142,7 @@ for a, acc, i in zip(np.arange(0.125,1.25,0.025), results_folders, range(num_col
     weights /= n_files
     bias /= n_files  
     wb = np.concatenate((weights.flatten(), bias.flatten()))
-    plt.errorbar(a, wb.mean(), yerr=wb.std(), color=str(colors[i]), alpha=1.)
+    plt.errorbar(a, wb.mean(), yerr=wb.std(), fmt='--o', color=str(colors[i]), alpha=1.)
 plt.savefig('./images/{}/errorbar_total_weights-accuracy({}-{}-step-{}).png'.format(topology,0.1, 1.0, step))
 plt.show()
 plt.close()    
@@ -163,7 +158,7 @@ for a, acc, i in zip(np.arange(0.125,1.25,0.025), results_folders, range(num_col
         weights += w[-2]
     weights /= n_files
     s_input_layer = weights.sum(axis=1)
-    plt.errorbar(a, s_input_layer.mean(), yerr=s_input_layer.std(), color=str(colors[i]), alpha=.5)
+    plt.errorbar(a, s_input_layer.mean(), yerr=s_input_layer.std(), fmt='--o', color=str(colors[i]), alpha=.5)
 plt.savefig('./images/{}/errorbar_node-strenght-accuracy({}-{}-step-{})-input.png'.format(topology,0.1, 1.0, step))
 plt.show()
 plt.close()   
@@ -181,7 +176,7 @@ for a, acc, i in zip(np.arange(0.125,1.25,0.025), results_folders, range(num_col
     weights /= n_files
     bias /= n_files
     s_output_layer = weights.sum(axis=0) + bias
-    plt.errorbar(a, s_output_layer.mean(), yerr=s_input_layer.std(), color=str(colors[i]), alpha=.5)
+    plt.errorbar(a, s_output_layer.mean(), yerr=s_input_layer.std(), fmt='--o', color=str(colors[i]), alpha=.5)
 plt.savefig('./images/{}/errorbar_node-strenght-accuracy({}-{}-step-{})-output.png'.format(topology,0.1, 1.0, step))
 plt.show()
 plt.close()   
@@ -199,7 +194,7 @@ for a, acc, i in zip(np.arange(0.125,1.25,0.025), results_folders, range(num_col
     weights += np.abs(np.min(weights))
     s_input_layer = weights.sum(axis=1)
     Y = np.sum(weights**2, axis=1)/s_input_layer**2
-    plt.errorbar(a, Y.mean(), yerr=Y.std(), color=str(colors[i]), alpha=.5)
+    plt.errorbar(a, Y.mean(), yerr=Y.std(), fmt='--o', color=str(colors[i]), alpha=.5)
 plt.savefig('./images/{}/errorbar_node-disparity-accuracy({}-{}-step-{})-input.png'.format(topology,0.1, 1.0, step))
 plt.show()
 plt.close()  
@@ -221,7 +216,7 @@ for a, acc, i in zip(np.arange(0.125,1.25,0.025), results_folders, range(num_col
     bias += min_
     s_output_layer = weights.sum(axis=0) + bias
     Y = (np.sum(weights**2, axis=0)+bias**2)/s_output_layer**2
-    plt.errorbar(a, Y.mean(), yerr=Y.std(), color=str(colors[i]), alpha=.5)
+    plt.errorbar(a, Y.mean(), yerr=Y.std(), fmt='--o', color=str(colors[i]), alpha=.5)
 plt.savefig('./images/{}/errorbar_node-disparity-accuracy({}-{}-step-{})-output.png'.format(topology,0.1, 1.0, step))
 plt.show()
 plt.close() 
@@ -245,4 +240,40 @@ params = [-0.34266558, 0.01597775**0.5, 580.,
           0.28088089, 0.01203812**0.5, 630.]
 plt.plot(x,trimodal(x,*params),color='red',lw=3,label='model')
 plt.legend()
+"""
+
+""" Fuzzy histograms (5 for each accuracy level)
+results_folders = ["0.1-0.125",
+                    "0.375-0.4",
+                    "0.575-0.6",
+                    "0.775-0.8",
+                    "0.95-0.975"]
+
+topology = 'fc'  
+step = 0.2
+bins = 100
+num_colors = len(results_folders)
+red = Color("green")
+colors = list(red.range_to(Color("red"),num_colors))
+nr_fuzzies = 5
+
+# HISTOGRAMS
+# Link weights histogram
+print("\n[logger]: Link weights histogram")
+for acc, i in zip(results_folders, range(num_colors)):
+    print("[logger]: Processing results {}".format(acc))
+    n_files = len(glob.glob("./results/{}/{}/*.npy".format(topology, acc)))
+    total_weights, total_bias = np.array([]), np.array([])
+    files_ = glob.glob("./results/{}/{}/*.npy".format(topology, acc))
+    rr = np.random.randint(0, len(files_), nr_fuzzies)
+    for r in rr:
+        w = np.load(files_[r], allow_pickle=True)
+        total_weights = np.append(w[-2].flatten(), total_weights)
+        total_bias = np.append(w[-1].flatten(), total_bias) 
+        density = stats.kde.gaussian_kde(np.concatenate((total_weights.flatten(), total_bias.flatten())))
+        x = np.arange(-1., 1., .001)
+        plt.plot(x, density(x), alpha=.5, color=str(colors[i]))
+plt.savefig('./images/{}/fuzzy_histograms-{}_total_weights-accuracy({}-{}-step-{}).png'.format(topology,nr_fuzzies,0.1, 1.0, step))
+plt.show()
+plt.close()
 """
